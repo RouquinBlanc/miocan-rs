202f29379e2e64673f4828f5f64e6a811e095d20
Jonathan Martin <therouquinblanc@gmail.com>
2017-06-28 16:43:50 +0000

    Moving back to using official socketcan-rs on version github@1.7.0

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use mio::{Poll, PollOpt, Ready, Token};
 use socketcan::{CANSocket, CANFrame};
 use mio::event::Evented;
 use mio::unix::EventedFd;
+use std::os::unix::io::AsRawFd;
 
 pub struct MIOCANSocket {
     socket: CANSocket
@@ -26,16 +27,16 @@ impl MIOCANSocket {
 impl Evented for MIOCANSocket {
     fn register(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt) -> io::Result<()>
     {
-        EventedFd(&self.socket.raw_fd()).register(poll, token, interest, opts)
+        EventedFd(&self.socket.as_raw_fd()).register(poll, token, interest, opts)
     }
 
     fn reregister(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt)
         -> io::Result<()>
     {
-        EventedFd(&self.socket.raw_fd()).reregister(poll, token, interest, opts)
+        EventedFd(&self.socket.as_raw_fd()).reregister(poll, token, interest, opts)
     }
 
     fn deregister(&self, poll: &Poll) -> io::Result<()> {
-        EventedFd(&self.socket.raw_fd()).deregister(poll)
+        EventedFd(&self.socket.as_raw_fd()).deregister(poll)
     }
 }
